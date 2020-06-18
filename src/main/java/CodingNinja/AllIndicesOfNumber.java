3adba9ee9b861f5b1eca9ac6e1956125fd5bc470
vijay.kumar <kumar.vijay696@gmail.com>
2020-06-18 22:42:55 +0530

    Adding AllIndicesOfNumber recursion

---
+++ src/main/java/CodingNinja/AllIndicesOfNumber.java
@@ -0,0 +1,19 @@
+package CodingNinja;
+
+public class AllIndicesOfNumber {
+
+    public static int[] allIndexes(int input[], int x) {
+        /* Your class should be named Solution
+         * Don't write main().
+         * Don't read input, it is passed as function argument.
+         * Return output and don't print it.
+         * Taking input and printing output is handled automatically.
+         */
+        return new int [1];
+
+    }
+
+    public static void main(String [] args){
+
+    }
+}
--- src/main/java/CodingNinja/LastIndexRecursion.java
+++ src/main/java/CodingNinja/LastIndexRecursion.java
@@ -1,4 +1,27 @@
 package CodingNinja;
 
 public class LastIndexRecursion {
+
+    public static int lastIndex(int input[], int startIndex, int x) {
+        if(startIndex >= input.length) {
+            return -1;
+        }
+
+        int index = lastIndex(input, startIndex+1, x);
+
+        if(input[startIndex] == x && index==-1 ){
+            return startIndex;
+        }
+        return index;
+    }
+
+    public static int lastIndex(int input[], int x) {
+        return lastIndex(input, 0, x);
+    }
+
+    public static void main(String [] args){
+        int arr [] = {9, 8, 10, 8};
+        int num = 7;
+        System.out.println(lastIndex(arr, num));
+    }
 }
